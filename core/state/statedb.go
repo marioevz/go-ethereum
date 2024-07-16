e586a560bd5a7c29937a6d10d2e883ad9e329b0b
lightclient <lightclient@protonmail.com>
2024-07-16 11:54:31 -0600

    core/state: consider 7702 delegations to exist

--- core/state/statedb.go
+++ core/state/statedb.go
@@ -327,6 +327,9 @@ func (s *StateDB) SubRefund(gas uint64) {
 // Exist reports whether the given account address exists in the state.
 // Notably this also returns true for self-destructed accounts.
 func (s *StateDB) Exist(addr common.Address) bool {
+	if _, ok := s.transientDelegation[addr]; ok {
+		return true
+	}
 	return s.getStateObject(addr) != nil
 }
 
